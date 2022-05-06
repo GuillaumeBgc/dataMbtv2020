51fa593145a2c435180814162ec0ade4188c4983
GuillaumeBgc <beguecguillaume@gmail.com>
2022-05-06 15:06:49 +0200

    add

--- data-raw/data_prep.R
+++ data-raw/data_prep.R
@@ -1,8 +1,18 @@
-decathlon <- read.csv("dataset/decathlon.csv", row.names=1, header=TRUE, sep=";")
+decathlon <- read.csv("dataset/decathlon.csv", row.names=1, header=TRUE, sep=";", dec=',')
 
-replace(decathlon[1:41,1:12], values = )
-decathlon[,1:12] <- as.numeric(decathlon[1:41,1:12])
-decathlon[1:41,1:12]
-for(i in 1:12){
-  decathlon[,i] <- as.numeric(gsub(",", ".", gsub("\\.", "", decathlon[,i])))
-}
+lipsticks <- read.csv("dataset/lipsticks.csv", sep=",")
+
+perfumes_qda_experts <- read.csv("dataset/perfumes_qda_experts.csv", sep=",")
+
+sensochoc <- read.csv("dataset/sensochoc.csv", sep=";")
+
+tea <- read.csv("dataset/tea.csv", sep=";")
+
+wine <- read.csv("dataset/wine.csv", sep=";", dec=',')
+
+use_data(decathlon, overwrite = TRUE)
+use_data(lipsticks, overwrite = TRUE)
+use_data(perfumes_qda_experts, overwrite = TRUE)
+use_data(sensochoc, overwrite = TRUE)
+use_data(tea, overwrite = TRUE)
+use_data(wine, overwrite = TRUE)
